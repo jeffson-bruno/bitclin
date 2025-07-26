f919c53fb42079671955882bb581b25f54b43113
jeffson-bruno <brunojeff0012@gmail.com>
2025-07-26 10:21:25 -0300

    Criando um Controller para  chamada de senha do Médico

--- app/Http/Controllers/Medico/ChamadaSenhaController.php
+++ app/Http/Controllers/Medico/ChamadaSenhaController.php
@@ -4,8 +4,26 @@ namespace App\Http\Controllers\Medico;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\SenhaAtendimento;
+use Illuminate\Support\Carbon;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class ChamadaSenhaController extends Controller
 {
     //
+    public function chamar($paciente_id)
+    {
+        $senha = SenhaAtendimento::where('paciente__id', $paciente_id)->latest()->first();
+
+        if (!$senha) {
+            return response()->json(['error' => 'Senha não encontrada.'], 404);
+        }
+
+        $senha->chamada_em = Carbon::now();
+        $senha->save();
+
+        return response()->json(['success' => true, 'mensagem' => 'Senha chamada com sucesso!']);
+    }
 }
