116892526e5ff654712011d425051bd7af71aea3
jeffson-bruno <brunojeff0012@gmail.com>
2025-07-27 07:27:23 -0300

    Atualizando o  MonitorController para  buscar dados  Dinâmicos

--- app/Http/Controllers/Admin/MonitorController.php
+++ app/Http/Controllers/Admin/MonitorController.php
@@ -11,4 +11,28 @@ class MonitorController extends Controller
     {
         return Inertia::render('Admin/MonitorChamadas');
     }
+
+    // Método novo: API para retornar a última chamada + histórico
+    public function dadosChamadas()
+    {
+        $ultimasChamadas = Chamada::with('senha')
+            ->latest()
+            ->take(3)
+            ->get()
+            ->reverse()
+            ->map(function ($chamada) {
+                return [
+                    'senha' => $chamada->senha->codigo ?? '---',
+                    'nome' => $chamada->senha->nome_paciente ?? 'Paciente',
+                    'chamada_em' => $chamada->created_at->format('H:i:s'),
+                ];
+            });
+
+        $chamadaAtual = $ultimasChamadas->last();
+
+        return response()->json([
+            'atual' => $chamadaAtual,
+            'ultimas' => $ultimasChamadas,
+        ]);
+    }
 }
