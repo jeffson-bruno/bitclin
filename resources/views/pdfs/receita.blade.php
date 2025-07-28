b805b5d7669f1655ccc4f61985ba12587483daf4
jeffson-bruno <brunojeff0012@gmail.com>
2025-07-28 17:40:04 -0300

    Atuaizando o Layout do Receita.blade

--- resources/views/pdfs/receita.blade.php
+++ resources/views/pdfs/receita.blade.php
@@ -2,150 +2,122 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Receita Médica</title>
     <style>
         body {
             font-family: DejaVu Sans, sans-serif;
-            font-size: 14px;
+            margin: 30px;
+            font-size: 13px;
             position: relative;
         }
 
-        .marca-dagua {
-            position: absolute;
-            top: 35%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            opacity: 0.07;
-            font-size: 80px;
-            white-space: nowrap;
-            z-index: 0;
+        .header {
+            text-align: center;
+            margin-bottom: 10px;
         }
 
-        .cabecalho, .rodape {
+        .header img {
+            max-height: 100px;
+            margin-bottom: 5px;
+        }
+
+        .title {
+            font-size: 20px;
+            font-weight: bold;
+            margin-bottom: 20px;
             text-align: center;
+            text-transform: uppercase;
+        }
+
+        .info {
+            margin-bottom: 15px;
+        }
+
+        .info p {
+            margin: 3px 0;
+        }
+
+        .medicamentos {
+            margin-top: 20px;
+        }
+
+        .medicamento-item {
             margin-bottom: 20px;
         }
 
-        .cabecalho img {
-            height: 60px;
-        }
-
-        .info-paciente {
-            margin-bottom: 20px;
-            z-index: 1;
-            position: relative;
-        }
-
-        .info-paciente p {
-            margin: 2px 0;
-        }
-
-        table.medicamentos {
-            width: 100%;
-            border-collapse: collapse;
-            margin-bottom: 30px;
-        }
-
-        table.medicamentos th, table.medicamentos td {
-            border: 1px solid #000;
-            padding: 8px;
-            text-align: left;
-        }
-
-        .assinatura {
-            margin-top: 50px;
-            text-align: center;
-        }
-
-        .assinatura p {
-            margin-top: 60px;
-            border-top: 1px solid #000;
+        .linha {
+            border-bottom: 1px solid #000;
             display: inline-block;
-            padding-top: 5px;
+            width: 100%;
         }
 
-        .rodape {
+        .footer {
             position: absolute;
             bottom: 30px;
             left: 0;
-            right: 0;
-            font-size: 12px;
-            color: #555;
+            width: 100%;
+            font-size: 11px;
+            text-align: center;
+            line-height: 1.4;
+        }
+
+        .assinatura {
+            margin-top: 60px;
+            text-align: right;
         }
     </style>
 </head>
 <body>
 
-    <!-- Marca d'água -->
-    <div class="marca-dagua">
-        BitClin
+    <div class="header">
+        <img src="{{ public_path('logo-clinica.png') }}" alt="Logo da Clínica">
     </div>
 
-    <!-- Cabeçalho -->
-    <div class="cabecalho">
-        <img src="{{ public_path('logo.png') }}" alt="Logomarca">
-        <h2>Clínica Médica BitClin</h2>
-        <p>Rua Projetada 01 - Cocal - PI</p>
-        <p>(86) 9 9907-8090 / (86) 9 9998-6493</p>
-    </div>
+    <div class="title">Receita Médica</div>
 
-    <!-- Informações do Paciente -->
-    <div class="info-paciente">
-        <p><strong>Paciente:</strong> {{ $paciente->nome }}</p>
+    <div class="info">
+        <p><strong>Nome do Paciente:</strong> {{ $paciente->nome }}</p>
         <p><strong>Idade:</strong> {{ \Carbon\Carbon::parse($paciente->data_nascimento)->age }} anos</p>
-        <p><strong>Data:</strong> {{ \Carbon\Carbon::now()->format('d/m/Y') }}</p>
-        <p><strong>Médico:</strong> Dr(a). {{ $medico->name }} | CRM: {{ $crm }}</p>
+        <p><strong>Data:</strong> {{ now()->format('d/m/Y') }}</p>
+        <p><strong>Médico:</strong> {{ $medico->name }}</p>
+        <p><strong>CRM:</strong> {{ $crm }}</p>
     </div>
 
-    <!-- Medicamentos -->
-    <table class="medicamentos">
-        <thead>
-            <tr>
-                <th>Nome</th>
-                <th>Tipo</th>
-                <th>Quantidade</th>
-                <th>Frequência</th>
-                <th>Observações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($medicamentos as $med)
-                <tr>
-                    <td>{{ $med['nome'] }}</td>
-                    <td>{{ $med['tipo'] }}</td>
-                    <td>{{ $med['quantidade'] }}</td>
-                    <td>
-                        @if (isset($med['vezesPorDia']) && is_array($med['vezesPorDia']))
-                            {{ implode('x ao dia, ', $med['vezesPorDia']) }}x ao dia
-                        @else
-                            {{ $med['vezesPorDia'] ?? '-' }}
-                        @endif
-                    </td>
-                    <td>
-                        @php
-                            $detalhes = $med['detalhes'] ?? [];
-                            echo implode(', ', array_filter([
-                                $detalhes['gotas'] ? $detalhes['gotas'] . ' gotas' : null,
-                                $detalhes['ml'] ? $detalhes['ml'] . ' ml' : null,
-                                $detalhes['comprimidos'] ? $detalhes['comprimidos'] . ' comprimidos' : null,
-                                $detalhes['ampolas'] ? $detalhes['ampolas'] . ' ampolas' : null,
-                            ]));
-                        @endphp
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="medicamentos">
+        @foreach($medicamentos as $med)
+            <div class="medicamento-item">
+                <p><strong>Nome/Remédio:</strong> <span class="linha">&nbsp;&nbsp;{{ $med['nome'] }}&nbsp;&nbsp;</span></p>
 
-    <!-- Assinatura -->
+                <p>
+                    <strong>Qnt Caixas:</strong> <span class="linha" style="width: 120px;">&nbsp;&nbsp;{{ $med['quantidade'] }}&nbsp;&nbsp;</span>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <strong>Nº Comprimidos/Gotas/Ml/Ampolas:</strong> 
+                    <span class="linha" style="width: 200px;">
+                        &nbsp;&nbsp;
+                        {{ $med['detalhes']['comprimidos'] ?? $med['detalhes']['gotas'] ?? $med['detalhes']['ml'] ?? $med['detalhes']['ampolas'] ?? '' }}
+                        &nbsp;&nbsp;
+                    </span>
+                </p>
+
+                <p>
+                    <strong>Quantidade de vezes ao dia:</strong> 
+                    <span class="linha" style="width: 150px;">
+                        &nbsp;&nbsp;{{ implode(' / ', $med['vezesPorDia']) }}x&nbsp;&nbsp;
+                    </span>
+                </p>
+            </div>
+        @endforeach
+    </div>
+
     <div class="assinatura">
-        <p>Assinatura e Carimbo do Médico</p>
+        ___________________________<br>
+        Assinatura / Carimbo Médico
     </div>
 
-    <!-- Rodapé -->
-    <div class="rodape">
-        Clínica BitClin &copy; {{ now()->year }} - Todos os direitos reservados.
+    <div class="footer">
+        Rua Projetada 01, Nº 123, Centro – Cocal-PI<br>
+        CEP 64230-000 – Telefone: (86) 9 9907-8090<br>
+        @bitclin.oficial | www.bitclin.com.br
     </div>
+
 </body>
 </html>
-
