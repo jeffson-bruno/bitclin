81b9e5d2959b9542f0310c6752b8c96e43f7865a
jeffson-bruno <brunojeff0012@gmail.com>
2025-07-28 16:31:35 -0300

    Criando a Model Receita

---
+++ app/Models/Receita.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Receita extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'paciente_id',
+        'medico_id',
+        'crm',
+        'arquivo',
+        'data_receita',
+    ];
+
+    // Relacionamentos
+    public function paciente()
+    {
+        return $this->belongsTo(Paciente::class);
+    }
+
+    public function medico()
+    {
+        return $this->belongsTo(User::class, 'medico_id');
+    }
+}
