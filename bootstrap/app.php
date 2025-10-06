586c65a5277a7623beacba1fdb0cce3e7ed5e03a
jeffson-bruno <brunojeff0012@gmail.com>
2025-10-06 17:00:26 -0300

    Registrando os Aliases

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+            'auth.admin' => \App\Http\Middleware\RedirectIfNotAdmin::class,
+            'auth.recepcionista' => \App\Http\Middleware\RedirectIfNotRecepcionista::class,
+            'auth.profissional' => \App\Http\Middleware\RedirectIfNotProfissional::class,
         ]);
 
         //
